fe3f13a70b5cd690c3fb006c1261cf5fa07afa13
Cliff Wulfman <cwulfman@princeton.edu>
2023-07-15 12:15:05 -0400

    working on CLI

--- main.py
+++ main.py
@@ -7,13 +7,35 @@ import typer
 from lrm_models import BaseGraph
 from translation_models import (
     Translation,
+    Translator,
     create_magazine_graph,
     create_publication_graph,
     create_translation_graph,
+    create_translator_graph,
 )
 
 
 app = typer.Typer(help="Spatrem CSV translator")
+
+
+@app.command()
+def generate_translation_graphs(indir: Path, outdir: Optional[Path] = None) -> None:
+    for csvfile in indir.iterdir():
+        print(f"processing {csvfile}")
+        outfile = csvfile.with_suffix(".ttl")
+        if outdir:
+            outfile = outdir / Path(outfile.name)
+        generate_graph(csvfile, outfile)
+
+
+@app.command()
+def generate_translator_graphs(indir: Path, outdir: Optional[Path] = None) -> None:
+    for csvfile in indir.iterdir():
+        print(f"processing {csvfile}")
+        outfile = csvfile.with_suffix(".ttl")
+        if outdir:
+            outfile = outdir / Path(outfile.name)
+        generate_translators_graph(csvfile, outfile)
 
 
 @app.command()
@@ -33,6 +55,25 @@ def generate_graph(infile: Path, outfile: Optional[Path] = None) -> None:
     else:
         with open(outfile, mode="w", encoding="utf-8-sig") as out:
             out.write(graphs.serialize())
+
+
+@app.command()
+def generate_translators_graph(infile: Path, outfile: Optional[Path] = None) -> None:
+    """generates rdf file from input csv file."""
+
+    translators = BaseGraph().graph
+    with open(infile, mode="r", encoding="utf-8-sig") as data:
+        reader: DictReader = DictReader(data, delimiter=";")
+        for row in reader:
+            record = Translator(**row)
+            translator = create_translator_graph(record)
+            translators += translator
+
+    if outfile is None:
+        stdout.write(translators.serialize())
+    else:
+        with open(outfile, mode="w", encoding="utf-8-sig") as out:
+            out.write(translators.serialize())
 
 
 if __name__ == "__main__":
